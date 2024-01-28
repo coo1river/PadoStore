7501a7a25d244eeb5d24fba2d64ff71b20743663
coo1river <longpaca@gmail.com>
2024-01-28 23:30:19 +0900

    🎨 style: produt 페이지 ui 세팅

---
+++ src/app/product/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { ProductMain } from "@/styles/productStyle";
+import React from "react";
+
+const Product: React.FC = () => {
+  return (
+    <ProductMain>
+      <h2>상품 페이지</h2>
+    </ProductMain>
+  );
+};
+
+export default Product;
