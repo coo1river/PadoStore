cd8f850993d8bc48a99931be830976db10168a7a
coo1river <longpaca@gmail.com>
2024-01-29 23:57:51 +0900

    🎨 style: 상품 디테일 페이지 ui 틀 잡기

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,6 +6,11 @@ const Product: React.FC = () => {
   return (
     <ProductMain>
       <h2>상품 페이지</h2>
+      <section className="recommend_product article_product">
+        <article>상품1</article>
+        <article>상품2</article>
+        <article>상품3</article>
+      </section>
     </ProductMain>
   );
 };
--- src/styles/productStyle.tsx
+++ src/styles/productStyle.tsx
@@ -1,6 +1,11 @@
 "use client";
 import styled from "styled-components";
+import { HomeMain } from "./homeStyle";
 
-export const ProductMain = styled.main`
-  width: 100%;
+export const ProductMain = styled(HomeMain)`
+  .article_product {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
 `;
