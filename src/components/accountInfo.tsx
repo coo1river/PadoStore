d56f1b0d598cd783d4b45de42db274e13848a224
coo1river <longpaca@gmail.com>
2024-02-08 01:45:55 +0900

    🎨 style: 입금 폼 퍼블리싱 일부 구현

--- src/components/accountInfo.tsx
+++ src/components/accountInfo.tsx
@@ -3,13 +3,28 @@ import { AccountInfoWrap } from "@/styles/accountInfoStyle";
 const AccountInfo: React.FC = () => {
   return (
     <AccountInfoWrap>
-      <h2>입금 정보 작성</h2>
-      <form>
+      <h2>입금 폼 작성</h2>
+      <form className="account_info_form">
         <label htmlFor="label-name">입금자 이름</label>
-        <input id="label-name" type="text" />
+        <input
+          id="label-name"
+          type="text"
+          placeholder="입금자 이름을 입력해 주세요"
+        />
 
-        <label htmlFor="label-name">입금자 </label>
-        <input id="label-name" type="text" />
+        <label htmlFor="label-account">입금자 계좌번호</label>
+        <input
+          id="label-account"
+          type="text"
+          placeholder="계좌번호를 입력해 주세요"
+        />
+
+        <label htmlFor="label-address">주소지 입력</label>
+        <input
+          id="label-address"
+          type="text"
+          placeholder="주소를 입력해 주세요"
+        />
       </form>
     </AccountInfoWrap>
   );
--- src/styles/accountInfoStyle.tsx
+++ src/styles/accountInfoStyle.tsx
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const AccountInfoWrap = styled.section``;
+export const AccountInfoWrap = styled.section`
+  margin-top: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0px 0px 4px 0px #b4b4b42d;
+
+  label {
+    font-size: 15px;
+  }
+
+  .account_info_form {
+    display: flex;
+    flex-direction: column;
+  }
+`;
