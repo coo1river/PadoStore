c6aae5ca75b6600499b42d4bdafde4a16d9cca4b
coo1river <longpaca@gmail.com>
2024-02-18 23:39:01 +0900

    ✨ feat: 아이디 조건 정규 표현식 추가, api 연결 추가

--- src/hooks/useValid.tsx
+++ src/hooks/useValid.tsx
@@ -1,10 +1,11 @@
+import emailValidApi from "@/api/emailValidApi";
 import idValidApi from "@/api/idValidApi";
 import { ReqData } from "@/api/joinApi";
 import { useState } from "react";
 
 interface Error {
   idErr: string | { user_id: string };
-  emailErr: string;
+  emailErr: string | { email: string };
   pwErr: string;
   pwCheckErr: string;
   userNameErr: string;
@@ -25,14 +26,22 @@ const useValid = (form: ReqData): Error => {
     userNameErr: "",
   });
 
+  // id 조건 정규 표현식
+  const idReg = /^[a-z]+[a-z0-9]{4,11}$/;
+
   // id 유효성 검사 함수
   const IdValid = async () => {
     if (!form.user_id) {
       setError({ ...error, idErr: "필수 입력 항목입니다." });
-    } else if (form.user_id.length < 2) {
-      setError({ ...error, idErr: "2자 이상 입력해 주세요." });
-    } else if (form.user_id.length > 13) {
-      setError({ ...error, idErr: "12자 이하의 아이디S만 가능합니다." });
+    }
+    if (!idReg.test(form.user_id)) {
+      setError({ ...error, idErr: "유효한 아이디 형식이 아닙니다." });
+    }
+    if (form.user_id.length < 2) {
+      setError({ ...error, idErr: "5자 이상 입력해 주세요." });
+    }
+    if (form.user_id.length > 13) {
+      setError({ ...error, idErr: "12자 이하의 아이디만 가능합니다." });
     } else {
       const IdValid = await idValidApi({ user_id: form.user_id });
       setError({ ...error, idErr: IdValid });
@@ -43,14 +52,15 @@ const useValid = (form: ReqData): Error => {
   const emailReg = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 
   // email 유효성 검사 함수
-  const EmailValid = () => {
+  const EmailValid = async () => {
     if (!form.email) {
       setError({ ...error, emailErr: "필수 입력 항목입니다." });
     }
     if (!emailReg.test(form.email)) {
       setError({ ...error, emailErr: "이메일 형식이 올바르지 않습니다." });
     } else {
-      setError({ ...error, emailErr: "" });
+      const emailValid = await emailValidApi({ email: form.email });
+      setError({ ...error, emailErr: emailValid });
     }
   };
 
