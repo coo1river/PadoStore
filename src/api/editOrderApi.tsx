341fa9ce0785a0ed585723f740a3ef5388f6ab12
coo1river <longpaca@gmail.com>
2024-04-25 23:42:41 +0900

    ✨ feat: 주문 상세 => 배송 정보 수정 시 주소 찾기 기능 추가 / 주문, 배송, 질문 수정 상태 관리 후 input으로 변경

---
+++ src/api/editOrderApi.tsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+interface User {
+  user_id: string;
+  addr_post: string;
+  addr: string;
+  addr_detail: string;
+  bank: string;
+  account_name: string;
+  account_number: string;
+}
+
+interface Order {
+  post_id: number;
+  purchase_user_id: string;
+  post_number: string;
+}
+
+interface Answer {
+  answer: string;
+  product_question_id: string;
+}
+
+interface OrderProduct {
+  purchase_product_name: string;
+  purchase_quantity: number;
+  purchase_price: number;
+  product_detail_id: number;
+}
+
+export interface OrderData {
+  order_id: number;
+  user: User;
+  order: Order;
+  answerList: Answer[];
+  orderProductList: OrderProduct[];
+}
+
+export default async function editOrderApi(data: OrderData) {
+  const url = "api/order/update";
+
+  try {
+    const res = await axios.put(url, data, {
+      headers: {
+        Authorization: sessionStorage.getItem("userToken"),
+      },
+    });
+    console.log("API 응답:", res.data);
+    return res.data;
+  } catch (error) {
+    console.error("API 오류:", error);
+    throw error;
+  }
+}
