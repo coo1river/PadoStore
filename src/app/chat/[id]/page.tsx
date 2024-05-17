ecc15ce4d61be7618a9573d40e20b01bb623f3ff
coo1river <longpaca@gmail.com>
2024-05-17 20:39:22 +0900

    🎨 style: 채팅 UI 구현

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
 import { ChatList, ChatMain, ChatRoom } from "@/styles/chatStyle";
+import ImgProfileBasic from "@/../public/assets/images/img-user-basic.png";
 import React from "react";
 
 export default function Chat() {
   return (
     <ChatMain>
-      <ChatList>리스트</ChatList>
-      <ChatRoom>채팅방</ChatRoom>
+      <ChatList>
+        <div className="user_wrap">
+          <img className="profile_img" src={ImgProfileBasic.src} />
+          <div className="nickname_chat_wrap">
+            <p className="nickname">알파카</p>
+            <p>감사합니다 잘 받았어요~</p>
+          </div>
+        </div>
+        <div className="user_wrap">
+          <img className="profile_img" src={ImgProfileBasic.src} />
+          <div className="nickname_chat_wrap">
+            <p className="nickname">거래123</p>
+            <p>혹시 이거 구매 가능한가요?</p>
+          </div>
+        </div>
+      </ChatList>
+      <ChatRoom>
+        <div className="message_other_wrap">
+          <img className="profile_image" src={ImgProfileBasic.src} />
+          <div className="chat message_other">안녕하세요! 구매 가능할까요?</div>
+          <div className="time_stamp">12:33</div>
+        </div>
+        <div className="message_self_wrap">
+          <div className="time_stamp">12:35</div>
+          <div className="chat message_self">넵 가능합니다</div>
+        </div>
+      </ChatRoom>
     </ChatMain>
   );
 }
--- src/styles/chatStyle.tsx
+++ src/styles/chatStyle.tsx
@@ -8,12 +8,82 @@ export const ChatMain = styled.main`
 `;
 
 export const ChatList = styled.article`
-  width: 300px;
-  background-color: lightblue;
+  width: 500px;
+  background-color: var(--color-trans-grey);
+  border-radius: 15px 0 0 10px;
+
+  .profile_img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .user_wrap {
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    border-bottom: 2px solid var(--color-lightgrey);
+  }
+
+  .nickname_chat_wrap {
+    line-height: 1.2rem;
+  }
+
+  .nickname {
+    font-weight: 600;
+  }
 `;
 
 export const ChatRoom = styled.article`
   width: 100%;
   box-sizing: border-box;
-  background-color: lightcoral;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  color: #424242;
+
+  .chat {
+    display: flex;
+    flex-direction: column;
+    padding: 13px 20px;
+    width: fit-content;
+    max-width: 60%;
+    margin: 10px 5px;
+    border-radius: 20px;
+    font-weight: 500;
+  }
+
+  .message_other_wrap {
+    display: flex;
+    align-items: center;
+  }
+
+  .profile_image {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    margin-left: 10px;
+  }
+
+  .time_stamp {
+    align-self: end;
+    font-size: 14px;
+    margin-bottom: 15px;
+  }
+
+  .message_other {
+    background-color: var(--color-trans-grey);
+  }
+
+  .message_self_wrap {
+    align-self: flex-end;
+    display: flex;
+    align-items: center;
+  }
+
+  .message_self {
+    background-color: var(--color-main);
+    color: white;
+  }
 `;
