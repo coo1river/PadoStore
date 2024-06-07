251c8265a599644036c3156860a97e634acd6bce
coo1river <longpaca@gmail.com>
2024-06-07 22:14:51 +0900

    ✨ feat: 채팅방 입장, 퇴장 api 코드 작성

---
+++ src/api/chat/chatEnterApi.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export interface chatRoomRes {
+  chat_room_id: number;
+  user_id: string;
+  user_status: string;
+}
+
+export default async function chatEnterApi(chat_room_id: string) {
+  const url = "/api/chat/enter";
+
+  try {
+    const res = await axios.post(url, {
+      headers: {
+        Authorization: sessionStorage.getItem("userToken"),
+      },
+      chat_room_id,
+    });
+    console.log("API 응답:", res.data);
+    return res.data;
+  } catch (error) {
+    console.error("API 오류:", error);
+    throw error;
+  }
+}
---
+++ src/api/chat/chatExitApi.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export default async function chatExitApi(chat_room_id: string) {
+  const url = "/api/chat/exit";
+
+  try {
+    const res = await axios.post(url, {
+      headers: {
+        Authorization: sessionStorage.getItem("userToken"),
+      },
+      chat_room_id,
+    });
+    console.log("API 응답:", res.data);
+    return res.data;
+  } catch (error) {
+    console.error("API 오류:", error);
+    throw error;
+  }
+}
