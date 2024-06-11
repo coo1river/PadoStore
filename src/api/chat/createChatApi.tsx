14cfa9688a4a38b2d861e3114d2b8c4ca15d70ed
coo1river <longpaca@gmail.com>
2024-06-11 23:38:29 +0900

    ✨ feat: createChatApi 수정

--- src/api/chat/createChatApi.tsx
+++ src/api/chat/createChatApi.tsx
@@ -13,11 +13,15 @@ export default async function createChatApi(receiver: string | string[]) {
   const url = "/api/chat/new";
 
   try {
-    const res = await axios.post(url, receiver, {
-      headers: {
-        Authorization: sessionStorage.getItem("userToken"),
-      },
-    });
+    const res = await axios.post(
+      url,
+      { receiver },
+      {
+        headers: {
+          Authorization: sessionStorage.getItem("userToken"),
+        },
+      }
+    );
     console.log("API 응답:", res.data);
     return res.data;
   } catch (error) {
