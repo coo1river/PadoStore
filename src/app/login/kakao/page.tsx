91ca27dcc0327835c509d895155bd0500940806a
coo1river <longpaca@gmail.com>
2025-03-25 03:49:45 +0900

    🐛 fix: 리액트 훅 에러 수정

--- src/app/login/kakao/page.tsx
+++ src/app/login/kakao/page.tsx
@@ -4,38 +4,43 @@ import useAuthStore from "@/store/useAuthStore";
 import { LoginMain } from "@/styles/loginStyle";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Kakao() {
   const router = useRouter();
+
   const { setToken } = useAuthStore();
 
+  const [token, setLocalToken] = useState<string | null>(null);
   const code =
     typeof window !== "undefined"
       ? new URL(window.location.href).searchParams.get("code")
       : null;
 
+  // 토큰 디코딩 및 페이지 이동 처리
+  useEffect(() => {
+    if (token) {
+      const decodedUserId = useDecodedToken(token);
+      if (decodedUserId) {
+        router.push(`/editProfile/${decodedUserId}`);
+      }
+    }
+  }, [token, router]);
+
+  // 카카오 로그인 요청 처리
   useEffect(() => {
     if (code) {
       axios
         .post("/api/sns/kakao", { code: code })
         .then((res) => {
-          // HTTP 상태 코드가 200인 경우에만 처리
           if (res.status === 200) {
             console.log("카카오 로그인 성공:", res.data);
-
-            // 새 토큰 저장
             const newToken = res.headers.authorization;
+
             setToken(newToken);
             sessionStorage.setItem("userToken", newToken);
 
-            // 토큰 디코딩
-            const decodedUserId = useDecodedToken(newToken);
-
-            // 사용자 정보로 라우팅
-            if (decodedUserId) {
-              router.push(`/editProfile/${decodedUserId}`);
-            }
+            setLocalToken(newToken);
           } else {
             console.error(
               "카카오 로그인 실패: 상태 코드가 200이 아님",
@@ -47,7 +52,7 @@ export default function Kakao() {
           console.error("카카오 로그인 실패:", error);
         });
     }
-  }, [code, router, setToken]);
+  }, [code, setToken]);
 
   return <LoginMain />;
 }
